3438999a841e597efd28c3f2402b6ef6412014be
Pham Manh Cuong <cuongpm.ninasoft@gmail.com>
2022-07-05 17:37:58 +0700

    demo User Signed-off-by: Pham Manh Cuong <cuongpm.ninasoft@gmail.com>
    Signed-off-by: Pham Manh Cuong <cuongpm.ninasoft@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,3 +24,11 @@ Route::post('admin/posts/store', [PostController::class, 'store'])->name('admin.
 Route::get('admin/posts/{id}/edit', [PostController::class, 'edit'])->name('admin.posts.edit');
 Route::post('admin/posts/{id}/update', [PostController::class, 'update'])->name('admin.posts.update');
 Route::get('admin/posts/{id}/destroy', [PostController::class, 'destroy'])->name('admin.posts.destroy');
+
+//users
+Route::get('admin/users/index', [UserController::class, 'index'])->name('admin.users.index');
+Route::get('admin/users/create', [UserController::class, 'create'])->name('admin.users.create');
+Route::post('admin/users/store', [UserController::class, 'store'])->name('admin.users.store');
+Route::get('admin/users/{id}/edit', [UserController::class, 'edit'])->name('admin.users.edit');
+Route::put('admin/users/{id}/update', [UserController::class, 'update'])->name('admin.users.update');
+Route::delete('admin/users/{id}/destroy', [UserController::class, 'destroy'])->name('admin.users.destroy');
