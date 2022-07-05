c9a9e9575c47971e292202b441baf283dfa95275
Pham Manh Cuong <cuongpm.ninasoft@gmail.com>
2022-07-05 18:12:26 +0700

    Edit user
    Signed-off-by: Pham Manh Cuong <cuongpm.ninasoft@gmail.com>

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -46,12 +46,35 @@ class UserController extends Controller
 
     public function edit(Request $request)
     {
+        $id = $request->id;
+        $user = User::find($id);
 
+        return view('admin.users.edit',compact('user'));
     }
 
     public function update(Request $request, $id)
     {
+        $request->validate([
+            'name' => 'required|unique:users|max:255',
+            'email' => 'required|unique:users|max:255|email',
+            'password' => 'required|min:6',
 
+        ],[
+            'name.required' => 'Bạn cần nhập tên tài khoản',
+            'email.required' => 'Bạn cần nhập email',
+            'password.required' => 'Bạn cần nhập mật khẩu',
+            'name.unique' => 'Tên tài khoản đã tồn tại',
+            'email.unique' => 'Email đã tồn tại',
+            'name.max' => 'Tên tối đa 255 ký tự',
+            'email.max' => 'Email tối đa 255 ký tự',
+            'password.min' => 'Mật Khẩu tối thiếu có 6 ký tự',
+            'email.email' => 'Bạn phải nhập địa chỉ email hợp lệ',
+        ]);
+        $data = $request->all();
+        $user = User::find($id);
+        $user->update($data);
+
+        return redirect()->route('admin.users.index')->with('success','Sửa thông tin tài khoản thành công !');
     }
 
     public function destroy($id)
--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -1,1 +1,40 @@
-
+@extends('admin.layout')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                @if ($errors->any())
+                    <div class="alert alert-default-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                <form method="post" action="{{ route('admin.users.update', ['id' => $user->id]) }}">
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title">Sửa</h3>
+                        </div>
+                        <div class="card-body">
+                            {{ method_field('PUT') }}
+                            @csrf
+                            <div class="form-group">
+                                <label for="name">Name:</label>
+                                <input class="form-control" type="text" value="{{ $user->name }}" name="name">
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email:</label>
+                                <input class="form-control" type="text" value="{{ $user->email }}" name="email">
+                            </div>
+                        </div>
+                        <div class="card-footer">
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
